6da3bb91e18fe8e129a1c5d56ae7ae6091540dd2
Husan <hrukhitdinov@gmail.com>
2024-08-10 16:47:58 +0500

    sds

--- authService/config/config.go
+++ authService/config/config.go
@@ -9,43 +9,45 @@ import (
 )
 
 type Config struct {
-	HTTP_PORT           string
-	USER_SERVICE        string
-	DB_HOST             string
-	DB_PORT             string
-	DB_USER             string
-	DB_PASSWORD         string
-	DB_NAME             string
-	SIGNING_KEY         string
-	REFRESH_SIGNING_KEY string
-	EMAIL               string
-	PASSWORD            string
+	Postgres PostgresConfig
+	Server   ServerConfig
 }
 
-func Load() Config {
+type PostgresConfig struct {
+	DB_HOST     string
+	DB_PORT     string
+	DB_USER     string
+	DB_NAME     string
+	DB_PASSWORD string
+}
+
+type ServerConfig struct {
+	USER_PORT string
+}
+
+func Load() *Config {
 	if err := godotenv.Load(".env"); err != nil {
-		log.Print("No .env file found?")
+		log.Printf("error while loading .env file: %v", err)
 	}
 
-	config := Config{}
-	config.HTTP_PORT = cast.ToString(Coalesce("HTTP_PORT", ":8075"))
-	config.USER_SERVICE = cast.ToString(Coalesce("USER_SERVICE", ":8081"))
-	config.DB_HOST = cast.ToString(Coalesce("DB_HOST", "postgres-db"))
-	config.DB_PORT = cast.ToString(Coalesce("DB_PORT", 5432))
-	config.DB_USER = cast.ToString(Coalesce("DB_USER", "postgres"))
-	config.DB_PASSWORD = cast.ToString(Coalesce("DB_PASSWORD", "3333"))
-	config.DB_NAME = cast.ToString(Coalesce("DB_NAME", "legueauth"))
-	config.SIGNING_KEY = cast.ToString(Coalesce("SIGNING_KEY", "secret"))
-	config.REFRESH_SIGNING_KEY = cast.ToString(Coalesce("REFRESH_SIGNING_KEY", "secret"))
-	// config.EMAIL = cast.ToString(Coalesce("EMAIL", "hrukhitdinov@gmail.com"))
-	// config.PASSWORD = cast.ToString(Coalesce("PASSWORD", "htyy mkpy wsrk brgg"))
-
-	return config
+	return &Config{
+		Postgres: PostgresConfig{
+			DB_HOST:     cast.ToString(coalesce("DB_HOST", "localhost")),
+			DB_PORT:     cast.ToString(coalesce("DB_PORT", "5432")),
+			DB_USER:     cast.ToString(coalesce("DB_USER", "postgres")),
+			DB_NAME:     cast.ToString(coalesce("DB_NAME", "any")),
+			DB_PASSWORD: cast.ToString(coalesce("DB_PASSWORD", "password")),
+		},
+		Server: ServerConfig{
+			USER_PORT: cast.ToString(coalesce("USER_PORT", ":50051")),
+		},
+	}
 }
 
-func Coalesce(key string, defaultValue interface{}) interface{} {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
+func coalesce(key string, value interface{}) interface{} {
+	val, exist := os.LookupEnv(key)
+	if exist {
+		return val
 	}
-	return defaultValue
+	return value
 }
